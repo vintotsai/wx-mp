2e0d8ae7999f485f7664674bb9b640e62be9b5aa
vintotsai <vintotsai@gmail.com>
2022-08-10 17:42:27 +0800

    init

---
+++ miniprogram/pages/logs/logs.ts
@@ -0,0 +1,25 @@
+// logs.ts
+// const util = require('../../utils/util.js')
+import { formatTime } from '../../utils/util'
+
+Page({
+  data: {
+    logs: [],
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../index/index',
+    })
+  },
+  onLoad() {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map((log: string) => {
+        return {
+          date: formatTime(new Date(log)),
+          timeStamp: log
+        }
+      }),
+    })
+  },
+})
